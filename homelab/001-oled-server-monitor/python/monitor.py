cf59280d58646561bcbdb1b5a0fae8e9731bbf2e
marpiech <piechota.marcin@gmail.com>
2023-01-11 12:44:14 +0100

    server monitor corrections

--- homelab/001-oled-server-monitor/python/monitor.py
+++ homelab/001-oled-server-monitor/python/monitor.py
@@ -1,15 +1,12 @@
 import psutil
 import statistics
 import time
-import json
 import serial
 
 # Initialize an empty list to store the data
 data = []
 interval = 10
-
-# Open serial port
-ser = serial.Serial('/dev/ttyACM0', 115200)
+cache_items = 200
 
 while True:
 
@@ -21,26 +18,56 @@ while True:
   network_sent_bytes = psutil.net_io_counters().bytes_sent
   network_recv_bytes = psutil.net_io_counters().bytes_recv
   temp = statistics.mean([a[1] for a in psutil.sensors_temperatures()["coretemp"]])
-  
+
   idx = len(data)
   if (idx > 0):
-    disk_read_mbs = round((disk_read_bytes - data[idx-1]["disk_read_bytes"]) / interval / 1024 / 1024, 2)
-    disk_write_mbs = round((disk_write_bytes - data[idx-1]["disk_write_bytes"]) / interval / 1024 / 1024, 2)
-    network_sent_mbs = round((network_sent_bytes - data[idx-1]["network_sent_bytes"]) / interval / 1024 / 1024, 2)
-    network_recv_mbs = round((network_recv_bytes - data[idx-1]["network_recv_bytes"]) / interval / 1024 / 1024, 2)
+    disk_read_mbs = round(8 * (disk_read_bytes - data[idx-1]["disk_read_bytes"]) / interval / 1024 / 1024, 2)
+    # calculate disk read speed as percent of 6 Gb/s (6000 MB/s), round and set to 100 if > 100 and to 0 if < 0
+    disk_read_percent = max(0, min(100, int(100 * disk_read_mbs / 12000)))
+    disk_write_mbs = round(8 * (disk_write_bytes - data[idx-1]["disk_write_bytes"]) / interval / 1024 / 1024, 2)
+    disk_write_percent = max(0, min(100, int(100 * disk_write_mbs / 12000)))
+    network_sent_mbs = round(8 * (network_sent_bytes - data[idx-1]["network_sent_bytes"]) / interval / 1024 / 1024, 2)
+    # calculate network sent speed as percent of 1 Gb/s (1000 MB/s), round and set to 100 if > 100 and to 0 if < 0
+    network_sent_percent = max(0, min(100, int(100 * network_sent_mbs / 1000)))
+    network_recv_mbs = round(8 * (network_recv_bytes - data[idx-1]["network_recv_bytes"]) / interval / 1024 / 1024, 2)
+    network_recv_percent = max(0, min(100, int(100 * network_recv_mbs / 1000)))
   else:
     disk_read_mbs = 0
+    disk_read_percent = 0
     disk_write_mbs = 0
+    disk_write_percent = 0
     network_sent_mbs = 0
+    network_sent_percent = 0
     network_recv_mbs = 0
+    network_recv_percent = 0
 
   # Get system information
   max_ram_gb = round(psutil.virtual_memory().total / 1024 / 1024 / 1024, 2)
   max_threads = psutil.cpu_count()
-  hdd_space_gb = round(psutil.disk_usage('/').total / 1024 / 1024 / 1024, 2) 
+  hdd_space_gb = round(psutil.disk_usage('/').total / 1024 / 1024 / 1024, 2)
   hdd_percent = round(100 * (psutil.disk_usage('/').used / 1024 / 1024 / 1024 ) / hdd_space_gb, 2)
 
   # Store the data point as a tuple
+  init_point = {"cpu_percent": 0,
+                "memory_percent": 0,
+                "disk_read_bytes": 0,
+                "disk_write_bytes": 0,
+                "network_sent_bytes": 0,
+                "network_recv_bytes": 0,
+                "disk_read_mbs": 0,
+                "disk_read_percent": 0,
+                "disk_write_mbs": 0,
+                "disk_write_percent": 0,
+                "network_sent_mbs": 0,
+                "network_sent_percent": 0,
+                "network_recv_mbs": 0,
+                "network_recv_percent": 0,
+                "max_ram_gb": 0,
+                "max_threads": 0,
+                "hdd_space_gb": 0,
+                "hdd_percent": 0,
+                "temp": 0}
+
   data_point = {"cpu_percent": cpu_percent,
                 "memory_percent": memory_percent,
                 "disk_read_bytes": disk_read_bytes,
@@ -48,24 +75,77 @@ while True:
                 "network_sent_bytes": network_sent_bytes,
                 "network_recv_bytes": network_recv_bytes,
                 "disk_read_mbs": disk_read_mbs,
+                "disk_read_percent": disk_read_percent,
                 "disk_write_mbs": disk_write_mbs,
+                "disk_write_percent": disk_write_percent,
                 "network_sent_mbs": network_sent_mbs,
+                "network_sent_percent": network_sent_percent,
                 "network_recv_mbs": network_recv_mbs,
+                "network_recv_percent": network_recv_percent,
                 "max_ram_gb": max_ram_gb,
                 "max_threads": max_threads,
                 "hdd_space_gb": hdd_space_gb,
                 "hdd_percent": hdd_percent,
                 "temp": temp}
+
   
+  if (idx == 0):
+    for i in range(cache_items - 1):
+      data.append(init_point)
   data.append(data_point)
 
-  # If the list has more than 1000 elements, remove the oldest data point
-  if len(data) > 1000:
+  # If the list has more than cache_items elements, remove the oldest data point
+  if len(data) > cache_items:
       data.pop(0)
 
-  # Serialize the data to JSON and sent to uart using serial.write
-  ser.write((json.dumps(data_point) + "\n").encode('utf-8')) 
+  # serialize cpu percent into ascii string
+  try:
+    # Open serial port
+    ser = serial.Serial('/dev/ttyACM0', 115200)
+    ascii_cpu = "".join([chr(int(obj["cpu_percent"]) + 33) for obj in data])
+    cpu_info = "cpu:CPU_" + str(max_threads) + "_threads "
+    ser.write((cpu_info + ascii_cpu + chr(0)).encode('utf-8'))
+    print("cpu " + str(len(ascii_cpu.encode('utf-8'))) + " " + ascii_cpu[-10:])
+    time.sleep(1)
 
-  # Sleep for 10 seconds
-  time.sleep(interval)
+    ascii_mem = "".join([chr(int(obj["memory_percent"]) + 33) for obj in data])
+    mem_info = "mem:RAM_" + str(int(max_ram_gb)) + "GB "
+    ser.write((mem_info + ascii_mem + chr(0)).encode('utf-8'))
+    print("mem " + str(len(ascii_mem.encode('utf-8'))) + " "  + ascii_mem[-10:])
+    time.sleep(1)
+
+    ascii_disk_read = "".join([chr(int(obj["disk_read_percent"]) + 33) for obj in data])
+    disk_read_info = "disk_read:HDD_read_12gbps "
+    ser.write((disk_read_info + ascii_disk_read + chr(0)).encode('utf-8'))
+    print("disk_read " + str(len(ascii_disk_read.encode('utf-8'))) + " "  + ascii_disk_read[-10:])
+    time.sleep(1)
+
+    ascii_disk_write = "".join([chr(int(obj["disk_write_percent"]) + 33) for obj in data])
+    disk_write_info = "disk_write:write "
+    ser.write((disk_write_info + ascii_disk_write + chr(0)).encode('utf-8'))
+    print("disk_write " + str(len(ascii_disk_write.encode('utf-8'))) + " "  + ascii_disk_write[-10:])
+    time.sleep(1)
+
+    ascii_network_sent = "".join([chr(int(obj["network_sent_percent"]) + 33) for obj in data])
+    network_sent_info = "network_sent:sent "
+    ser.write((network_sent_info + ascii_network_sent + chr(0)).encode('utf-8'))
+    print("network_sent " + str(len(ascii_network_sent.encode('utf-8'))) + " "  + ascii_network_sent[-10:])
+    time.sleep(1)
+
+    ascii_network_recv = "".join([chr(int(obj["network_recv_percent"]) + 33) for obj in data])
+    network_recv_info = "network_recv:Network_recvd_1Gbps "
+    ser.write((network_recv_info + ascii_network_recv + chr(0)).encode('utf-8'))
+    print("network_recv " + str(len(ascii_network_recv.encode('utf-8'))) + " "  + ascii_network_recv[-10:])
+    time.sleep(1)
+
+    ascii_temp = "".join([chr(int(obj["temp"]) + 33) for obj in data])
+    temp_info = "temp:CPU_temp_C "
+    ser.write((temp_info + ascii_temp + chr(0)).encode('utf-8'))
+    print("temp " + str(len(ascii_temp.encode('utf-8'))) + " "  + ascii_temp[-10:])
+
+    time.sleep(4)
+    ser.close()
+  except:
+    print("Serial port error")
+    time.sleep(10)
 
--- homelab/001-oled-server-monitor/src/tft.ino.cpp
+++ homelab/001-oled-server-monitor/src/tft.ino.cpp
@@ -1,12 +1,61 @@
 #include "TFT_eSPI.h"
 #include "FreeSansBold7pt7b.h"
+#include "ArduinoJson-v6.20.0.h"
+#include <stdio.h>
+#include <string.h>
+#include <regex.h>
+
+#define START_BYTE 0x01
+#define END_BYTE 0x00
+#define ARRAY_SIZE 200
 
 TFT_eSPI tft= TFT_eSPI();
 TFT_eSprite legend = TFT_eSprite(&tft);
 TFT_eSprite plot = TFT_eSprite(&tft);
 
-word convertRgb( byte R, byte G, byte B)
-{
+int oled_width = 320;
+int oled_height = 170;
+int legend_width = 95;
+int margin = 5;
+int num_points = ARRAY_SIZE;
+
+int cpu[ARRAY_SIZE];
+int mem[ARRAY_SIZE];
+int net_in[ARRAY_SIZE];
+int net_out[ARRAY_SIZE];
+int hdd_read[ARRAY_SIZE];
+int hdd_write[ARRAY_SIZE];
+int temp[ARRAY_SIZE];
+
+String cpu_desc = "CPU";
+String mem_desc = "RAM";
+String net_in_desc = "NET in";
+String net_out_desc = "NET out";
+String hdd_read_desc = "HDD read";
+String hdd_write_desc = "HDD write";
+String temp_desc = "TEMP";
+
+// get start time
+unsigned long last_update = millis() - 20000;
+
+// fill all the arrays with -1 values
+void fillArray(int array[], int size) {
+  for (int i = 0; i < size; i++) {
+    array[i] = 101;
+  }
+}
+
+char receivedChars[ARRAY_SIZE + 35];
+boolean newData = false;
+String error = "no error";
+String message = "Serial not available";
+String result = "Serial not available";
+int message_length = 0;
+int iteration = 0;
+
+// define colors
+
+word convertRgb( byte R, byte G, byte B) {
   return ( ((R & 0xF8) << 8) | ((G & 0xFC) << 3) | (B >> 3) );
 }
 
@@ -24,97 +73,290 @@ word convertRgb( byte R, byte G, byte B)
 #define darkViolet convertRgb(0x59, 0x50, 0x7B)
 #define lightViolet convertRgb(0x98, 0x83, 0xA3)
 
+void plotLegend() {
+  legend.setTextDatum(3);
+  int rect_width = legend_width - 2 * margin;
+  int rect_height = 20;
+  int round = 4;
+  int rect_x = margin;
+  int rect_y = margin;
+  rect_y = margin + 5 + 0 * (rect_height + 5);
+  legend.fillRoundRect(rect_x,rect_y,rect_width,rect_height,round,darkGreen);
+  legend.drawString("CPU", margin + 10, rect_y + 9);
+  rect_y = margin + 5 + 1 * (rect_height + 5);
+  legend.fillRoundRect(rect_x,rect_y,rect_width,rect_height,round,lightGreen);
+  legend.drawString("RAM", margin + 10, rect_y + 9);
+  rect_y = margin + 5 + 2 * (rect_height + 5);
+  legend.fillRoundRect(rect_x,rect_y,rect_width,rect_height,round,darkOrange);
+  legend.drawString("NET in", margin + 10, rect_y + 9);
+  rect_y = margin + 5 + 3 * (rect_height + 5);
+  legend.fillRoundRect(rect_x,rect_y,rect_width,rect_height,round,lightOrange);
+  legend.drawString("NET out", margin + 10, rect_y + 9);
+  rect_y = margin + 5 + 4 * (rect_height + 5);
+  legend.fillRoundRect(rect_x,rect_y,rect_width,rect_height,round,darkRed);
+  legend.drawString("HDD write", margin + 10, rect_y + 9);
+  rect_y = margin + 5 + 5 * (rect_height + 5);
+  legend.fillRoundRect(rect_x,rect_y,rect_width,rect_height,round,lightRed);
+  legend.drawString("HDD read", margin + 10, rect_y + 9);
+}
+
+// setup
 
 
 
 void setup() {
 
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   tft.init();
   tft.setRotation(1);
   tft.setSwapBytes(true);
   tft.fillScreen(dark);
 
-  legend.createSprite(120,170); 
+  legend.createSprite(legend_width,oled_height); 
   legend.setTextColor(light);
   legend.setFreeFont(&FreeSansBold7pt7b);
-  legend.setTextDatum(3);
   legend.fillSprite(dark);
-
-  legend.fillRoundRect(5,10,105,20,4,darkGreen);
-  legend.drawString("CPU " + String(4) + " cores", 15, 19);
-  legend.fillRoundRect(5,35,105,20,4,lightGreen);
-  legend.drawString("RAM " + String(4) + " GB", 15, 44);
-  legend.fillRoundRect(5,60,105,20,4,darkOrange);
-  legend.drawString("NET in", 15, 69);
-  legend.fillRoundRect(5,85,105,20,4,lightOrange);
-  legend.drawString("NET out", 15, 94);
-  legend.fillRoundRect(5,110,105,20,4,darkRed);
-  legend.drawString("HDD write", 15, 119);
-  legend.fillRoundRect(5,135,105,20,4,lightRed);
-  legend.drawString("HDD read", 15, 144);
+  plotLegend();
   legend.pushSprite(0,0);
 
-  plot.createSprite(200,170);
+  plot.createSprite(oled_width - legend_width,oled_height);
   plot.setTextColor(light);
   plot.fillSprite(dark);
   plot.setFreeFont(&FreeSansBold7pt7b);
-  plot.setTextDatum(2);
+  plot.pushSprite(legend_width,0);
 
-  plot.drawFastHLine(5, 10, 5, gray);
-  plot.drawFastHLine(5, 155, 5, gray);
-  plot.drawFastHLine(190, 10, 5, gray);
-  plot.drawFastHLine(190, 155, 5, gray);
-  // then plot the y axis
-  plot.drawFastVLine(5, 10, 145, gray);
-  plot.drawFastVLine(195, 10, 145, gray);
-  // the plot the array using loop
-  
-  int left_margin = 6;
-  int bottom = 155;
-  int num_points = 190;
-  int x_data[num_points];
-  int y_data1[num_points];
-  int y_data2[num_points];
-  for (int i = 0; i < num_points; i++) {
-    x_data[i] = i + left_margin;
-    if (i == 0) {
-      y_data1[i] = 100;
-      y_data2[i] = 60;
-    } else {
-      y_data1[i] = y_data1[i - 1] + random(-3, 3);
-      y_data2[i] = y_data2[i - 1] + random(-2, 3);
-      plot.fillRect(x_data[i - 1], bottom - y_data1[i], 1, y_data1[i], darkGreen);
-      plot.fillRect(x_data[i - 1], bottom - y_data2[i], 1, y_data2[i], lightGreen);
-      plot.drawLine(x_data[i - 1], bottom - y_data1[i], x_data[i], bottom - y_data1[i], darkGreen);
-      plot.drawLine(x_data[i - 1], bottom - y_data1[i] + 1, x_data[i], bottom - y_data1[i] + 1, darkGreen);
-      plot.drawLine(x_data[i - 1], bottom - y_data1[i] + 2, x_data[i], bottom - y_data1[i] + 2, darkGreen);
-      plot.drawLine(x_data[i - 1], bottom - y_data2[i], x_data[i], bottom - y_data2[i], lightGreen);
-      plot.drawLine(x_data[i - 1], bottom - y_data2[i] + 1, x_data[i], bottom - y_data2[i] + 1, lightGreen);
-      plot.drawLine(x_data[i - 1], bottom - y_data2[i] + 2, x_data[i], bottom - y_data2[i] + 2, lightGreen);
-    }
-  }
-
-  plot.pushSprite(120,0);
-  
-
+  fillArray(cpu, ARRAY_SIZE);
+  fillArray(mem, ARRAY_SIZE);
+  fillArray(net_in, ARRAY_SIZE);
+  fillArray(net_out, ARRAY_SIZE);
+  fillArray(hdd_read, ARRAY_SIZE);
+  fillArray(hdd_write, ARRAY_SIZE);
+  fillArray(temp, ARRAY_SIZE);
 
 }
 
-void loop() {
+void plotAxis() {
+  int right_axis = ARRAY_SIZE + margin - 1;
+  int bracket = 5;
+  plot.drawFastHLine(margin, 10, bracket, gray);
+  plot.drawFastHLine(margin, 155, bracket, gray);
+  plot.drawFastHLine(right_axis - bracket, 10, bracket, gray);
+  plot.drawFastHLine(right_axis - bracket, 155, bracket, gray);
+  plot.drawFastVLine(margin, 10, 145, gray);
+  plot.drawFastVLine(right_axis, 10, 145, gray);
+}
 
-  if (Serial.available() > 0) {
+String replace_char(String str, char find, char replace){
+    //convert String to char*
+    int len = str.length() + 1;
+    char array_char[len];
+    str.toCharArray(array_char, str.length() + 1);
+    char *current_pos = strchr(array_char,find);
+    while (current_pos) {
+        *current_pos = replace;
+        current_pos = strchr(current_pos,find);
+    }
+    return String(array_char);
+}
 
-    String message = Serial.readStringUntil('\n');
+void plotLineFill(int* y_data1, int* y_data2, int color1, int color2, String text1, String text2) {
+  int left_margin = 6;
+  int bottom = 155;
+  int x_data[ARRAY_SIZE];
 
-    plot.fillSprite(dark);
-    plot.fillRoundRect(10,10,100,100,4,darkBlue);
-    plot.drawString("->: " + message,50,30);
-    plot.pushSprite(0,0);
+  // set maximum for y_data1 to 100 minimu to 0 and scale y_data1 100 to 145;
+  int last_x = left_margin;
+  int last_y1 = int(map(max(min(y_data1[0], 100), 0), 0, 100, 0, 145));
+  int last_y2 = int(map(max(min(y_data2[0], 100), 0), 0, 100, 0, 145));
+  int x = last_x;
+  int y1 = last_y1;
+  int y2 = last_y2;
+
+  for (int i = 1; i < ARRAY_SIZE - 2; i++) {
+    x = i + left_margin;
+    y1 = int(map(max(min(y_data1[i], 100),0), 0, 100, 0, 145));
+    y2 = int(map(max(min(y_data2[i], 100),0), 0, 100, 0, 145));
+  
+    plot.fillRect(last_x, bottom - y1, 2, y1, color1);
+    plot.fillRect(last_x, bottom - y2, 2, y2, color2);
+    plot.drawLine(last_x, bottom - last_y1 - 1, x, bottom - y1 - 1, color1);
+    plot.drawLine(last_x, bottom - last_y1 - 2, x, bottom - y1 - 2, color1);
+    plot.drawLine(last_x, bottom - last_y1 - 3, x, bottom - y1 - 3, color1);
+    plot.drawLine(last_x, bottom - last_y2 - 1, x, bottom - y2 - 1, color2);
+    plot.drawLine(last_x, bottom - last_y2 - 2, x, bottom - y2 - 2, color2);
+    plot.drawLine(last_x, bottom - last_y2 - 3, x, bottom - y2 - 3, color2);
+
+    last_x = x;
+    last_y1 = y1;
+    last_y2 = y2;
+  }
+  // set text datum align to left and baseline
+  plot.setTextDatum(3);
+  plot.setTextColor(color1);
+  plot.drawString(replace_char(text1, '_', ' '), margin + 10, 15);
+  // set text datum align to right and baseline
+  plot.setTextDatum(5);
+  plot.setTextColor(color2);
+  plot.drawString(replace_char(text2, '_', ' '), ARRAY_SIZE + margin - 10, 15);
+}
+
+// fill array with ints based on ascii values
+void fillArrayWithAscii(int* array, String ascii_string) {
+  for (int i = 0; i < ARRAY_SIZE; i++) {
+    if (ascii_string.length() > i) { 
+      char c = ascii_string.charAt(i);
+      int ascii = (int) c - 33;
+      array[i] = ascii;
+    }    
+  }
+}
+
+
+void recvWithStartEndMarkers() {
+  static boolean recvInProgress = false;
+  static int ndx = 0;
+
+  char rc;
+  error = "serial not available";
+  // wait for serial data
+  while (Serial.available() == 0) {
+    error = "serial not available";
+    delay(50);
+    // if time from last update is greater than 20 seconds print an error message
+    int time_since_last_update = int((millis() - last_update) / 1000);
+    if (time_since_last_update > 20) {
+      plot.fillSprite(dark);
+      plot.setTextDatum(3);
+      plot.drawString("NO DATA FOR " + String(time_since_last_update) + "s",20,75);
+      plot.pushSprite(legend_width,0);
+      
+    }
 
   }
+  if (Serial.available() > 0) {
+    last_update = millis();
+    error = "Start";
+    recvInProgress = true;
+  }
+  int chunkId = 0;
+  while (recvInProgress) {
+    last_update = millis();
+    error = "recvInProgress";
+    if (Serial.available() > 0) {
+      error = "receiving";
+      rc = Serial.read();
+      if (ndx == 0) {
+        recvInProgress = true;
+      }
+      if (rc != END_BYTE) {
+        error = "not end marker";
+        receivedChars[ndx] = rc;
+        ndx++;
+        message_length = ndx;
+      }
+      else {
+        error = "terminated";
+        String msg = String(receivedChars);
+        int splitIndex = msg.indexOf(' ');
+        String desc = msg.substring(0, splitIndex);
+        int descSplitIndex = desc.indexOf(':');
+        String word = desc.substring(0, descSplitIndex);
+        desc = desc.substring(descSplitIndex + 1, desc.length());
+        String asciiCharacters = msg.substring(splitIndex + 1, splitIndex + 1 + ARRAY_SIZE);
+        message = word;
+        //get size of ascii characters
+        int size = asciiCharacters.length();
+        result = String(size) + " " + asciiCharacters.substring(size - 10, size);
+        result = word + " " + String(cpu[0]) + " " + String(cpu[1]) + " " + String(cpu[2]) + " " + String(cpu[3]);
+        if (word == "cpu") {
+          cpu_desc = desc;
+          fillArrayWithAscii(cpu, asciiCharacters);
+        }
+        if (word == "mem") {
+          mem_desc = desc;
+          fillArrayWithAscii(mem, asciiCharacters);
+        }
+        if (word == "network_recv") {
+          net_in_desc = desc;
+          fillArrayWithAscii(net_in, asciiCharacters);
+        }
+        if (word == "network_sent") {
+          net_out_desc = desc;
+          fillArrayWithAscii(net_out, asciiCharacters);
+        }
+        if (word == "disk_read") {
+          hdd_read_desc = desc;
+          fillArrayWithAscii(hdd_read, asciiCharacters);
+        }
+        if (word == "disk_write") {
+          hdd_write_desc = desc;
+          fillArrayWithAscii(hdd_write, asciiCharacters);
+        }
+        if (word == "temp") {
+          temp_desc = desc;
+          fillArrayWithAscii(temp, asciiCharacters);
+        }
 
-  delay(1000); 
+        recvInProgress = false;
+        ndx = 0;
+      }
+    }
+    delay(1);
+  }
+  chunkId++;
+  error = "chunks received: " + String(chunkId);
+  
+
+}
+
+int presentation = 0;
+
+void loop() {
+
+  recvWithStartEndMarkers();
+
+      
+  plot.fillSprite(dark);
+  plotAxis();
+  // if cpu is not empty, plot it
+  if (cpu[0] < 101 && mem[0] < 101 && presentation % 4 == 0) {
+    message = "printing cpu";
+    plotLineFill(mem, cpu, lightGreen, darkGreen, mem_desc, cpu_desc);
+  }
+  if (net_in[0] < 101 && net_out[0] < 101 && presentation % 4 == 1) {
+    message = "printing network";
+    plotLineFill(net_in, net_out, darkOrange, lightOrange, net_in_desc, net_out_desc);
+  }
+  if (hdd_read[0] < 101 && hdd_write[0] < 101 && presentation % 4 == 2) {
+    message = "printing disk";
+    plotLineFill(hdd_read, hdd_write, darkRed, lightRed, hdd_read_desc, hdd_write_desc);
+  }
+  if (temp[0] < 101 && presentation % 4 == 3) {
+    message = "printing temp";
+    plotLineFill(temp, temp, darkViolet, darkViolet, temp_desc, "");
+  }
+  
+  
+  plot.setTextDatum(3);
+  // plot.drawString("Result: " + result,20,50);
+  // plot.drawString("Ms: " + message + ".",20,75);
+  // plot.drawString("Length: " + String(message_length),20,100);
+  // plot.drawString("Iteration: " + String(iteration),20,125);
+  // plot.drawString("Error: " + error,20,150);
+  plot.pushSprite(legend_width,0);
+
+  iteration++;
+  if (iteration % 10 == 0) {
+    presentation++;
+    if (presentation > 3) {
+      presentation = 0;
+    }
+  }
+  // reset iteration if closing to int overflow
+  if (iteration > 10000000) {
+    iteration = 0;
+  }
+  delay(10); 
 
 }
